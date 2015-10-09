061c7abc1bb130300cb27d7dae8827465a37afd4
Nathan Reed <nathaniel.reed@gmail.com>
2015-10-08 17:36:27 -0700

    Add comments to variables in timer.h

--- timer.h
+++ timer.h
@@ -8,13 +8,13 @@ namespace Framework
 				Timer();
 		void	OnFrameStart();
 
-		float	m_timestep;
-		float	m_time;
-		int		m_frameCount;
+		float	m_timestep;					// Delta time in seconds between frames, averaged over last few frames
+		float	m_time;						// Time in seconds since startup
+		int		m_frameCount;				// Frames since startup
 
-		i64		m_startupTimestamp;
-		i64		m_lastFrameTimestamps[3];
-		int		m_iFrameCur;
-		float	m_period;
+		i64		m_startupTimestamp;			// QPC time of startup
+		i64		m_lastFrameTimestamps[3];	// Ring buffer of QPC times of last few frames
+		int		m_iFrameCur;				// Write index into ring buffer
+		float	m_period;					// QPC period in seconds
 	};
 }
